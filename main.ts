fd60b59050a4c426b5280b8f37cd936aa8424e4d
hielkebaron <hielkebaron@gmail.com>
2022-05-15 13:37:22 +0200

    V1

--- main.ts
+++ main.ts
@@ -1,3 +1,32 @@
-basic.forever(function () {
-	
-})
+/**
+ * Functions are mapped to blocks using various macros
+ * in comments starting with %. The most important macro
+ * is "block", and it specifies that a block should be
+ * generated for an **exported** function.
+ */
+
+//% color="#AA278D" weight=100
+namespace hello {
+    //% block
+    export function motor_forward() {
+        pins.digitalWritePin(DigitalPin.P12, 0)
+        pins.digitalWritePin(DigitalPin.P13, 1)
+        pins.analogWritePin(AnalogPin.P8, 1023)
+        pins.analogWritePin(AnalogPin.P1, 600)
+
+    }
+
+
+
+    // note that Caml casing yields lower case
+    // block text with spaces
+
+    //% block   
+    export function motor_reverse() {
+        pins.digitalWritePin(DigitalPin.P12, 1)
+        pins.digitalWritePin(DigitalPin.P13, 0)
+        pins.analogWritePin(AnalogPin.P8, 1023)
+        pins.analogWritePin(AnalogPin.P1, 600)
+
+    }
+}
